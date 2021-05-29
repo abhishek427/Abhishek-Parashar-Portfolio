2d90b04b2712b99f3b40fd9b2b4df7216326453c
abhishek427 <abhishek427821@gmail.com>
2021-05-29 15:11:19 +0530

    about me changes

--- src/about-me/AboutMe.js
+++ src/about-me/AboutMe.js
@@ -3,45 +3,34 @@ import "./AboutMe.css";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+import myImage from "../images/profile-1.jpeg";
+import Button from "@material-ui/core/Button";
 
 const AboutMe = () => {
-  const classes = useStyles();
   return (
     <div>
-      <Paper elevation={3} className="paper-size">
+      <Paper elevation={3} className="paper-size about-me-height">
         <Grid container justify="center">
-          <Grid xs={12}>
-            <Typography variant="h4" style={{ marginBottom: "5px" }}>
-              About me
-            </Typography>
+          <Grid xs={12} className="heading-alignment heading-height">
+            <Typography variant="h4">About me</Typography>
           </Grid>
 
           <Grid item xs={4}>
-            <Card className={classes.root}>
-              <CardContent></CardContent>
-            </Card>
+            <Paper elevation={5} className="my-image-paper-size">
+              <img src={myImage} className="image-size" />
+            </Paper>
           </Grid>
-          <Grid item xs={8}></Grid>
+          <Grid item xs={8}>
+            <div className="about-me-wrapper">
+              <div className="side-spacing">I am a developer</div>
+              <div className="side-spacing">
+                <Typography variant="inherit">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</Typography>
+              </div>
+              <div className="side-spacing">
+                <Button variant="contained">Contained</Button>
+              </div>
+            </div>
+          </Grid>
         </Grid>
       </Paper>
     </div>
