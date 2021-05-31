ac7ab71528c228729ecf5358fb8d268650356528
abhishek427 <abhishek427821@gmail.com>
2021-05-31 17:37:43 +0530

    back to top add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AboutMe from "./about-me/AboutMe";
 import MyServices from "./my-services/MyServices";
@@ -6,7 +6,23 @@ import MySkills from "./my-skills/MySkills";
 import ContactMe from "./contact-me/ContactMe";
 import MyProject from "./my-projects/MyProject";
 import Footer from "./footer/Footer";
+import { FaArrowCircleUp } from "react-icons/fa";
 function App() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  window.addEventListener("scroll", checkScrollTop);
   return (
     <div className="item-position">
       <AboutMe />
@@ -15,6 +31,7 @@ function App() {
       <MyProject />
       <ContactMe />
       <Footer />
+      <FaArrowCircleUp className="scrollTop" onClick={scrollTop} style={{ height: 40, display: showScroll ? "flex" : "none", color: "crimson" }} />
     </div>
   );
 }
